868520c45eab9badc4b843b9088e041134d947d2
Jasper Reddin <jazzbeenjamin@me.com>
2016-09-10 19:33:27 -0500

    Fix issue with going to flat file even when SQL works

--- src/main/java/com/drizzard/usernamerestrictor/UsernameRestrictor.java
+++ src/main/java/com/drizzard/usernamerestrictor/UsernameRestrictor.java
@@ -2,9 +2,7 @@ package com.drizzard.usernamerestrictor;
 
 import com.drizzard.usernamerestrictor.handler.SQLHandler;
 import com.drizzard.usernamerestrictor.handler.SettingsManager;
-
 import net.md_5.bungee.api.ChatColor;
-
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -21,129 +19,128 @@ import java.util.List;
 import java.util.logging.Level;
 
 public class UsernameRestrictor extends JavaPlugin implements Listener {
-   
+
 	private static UsernameRestrictor instance;
 
-    SettingsManager settings = SettingsManager.getInstance();
-    
-    @Override
-    public void onLoad() {
-    	
-        setInstance();
+	SettingsManager settings = SettingsManager.getInstance();
 
-        settings.setup(this);
+	@Override
+	public void onLoad() {
 
-        PluginDescriptionFile pluginDescriptionFile = this.getDescription();
-        Bukkit.getLogger().log(Level.INFO, String.format("Loaded %s version %s by RoyalNinja.",
-                pluginDescriptionFile.getName(),
-                pluginDescriptionFile.getVersion())
-        );
-    }
+		setInstance();
 
-    public void onEnable() {
-    	
-    	SQLHandler.setupMySQL();
-    	
-    	registerCommands();
-        registerHandlers();
-        registerEvents();
-    }
-    public void onDisable() {
-    	SQLHandler.cleanupConnection();
-    }
-   
-    public void registerCommands() {
-    
-    	getCommand("urestrictor").setExecutor(new RestrictorCommands());
-    	
-    }
+		settings.setup(this);
 
-    public void registerHandlers() {
-        
-        if (settings.getSQLData().getConfigurationSection("MySQL") == null) {
-        	settings.getSQLData().set("MySQL.Enabled", false);
-        	settings.getSQLData().set("MySQL.IP", "ip");
-        	settings.getSQLData().set("MySQL.Port", "port");
-        	settings.getSQLData().set("MySQL.Database", "database");
-        	settings.getSQLData().set("MySQL.User", "user");
-        	settings.getSQLData().set("MySQL.Password", "pass");
-        	settings.saveSQLData();
-        }
-        
-        if (settings.getPlayerData().getList("KnownUsernames") == null) {
-            settings.getPlayerData().set("KnownUsernames", new ArrayList<>());
-            settings.savePlayerData();
-        }
-        if (settings.getPlayerData().getString("KickMessage") == null) {
-        	settings.getPlayerData().set("KickMessage", "&cYou may not change your name! Original name: &b%name%");
-        }
+		PluginDescriptionFile pluginDescriptionFile = this.getDescription();
+		Bukkit.getLogger().log(Level.INFO, String.format("Loaded %s version %s by RoyalNinja.",
+				pluginDescriptionFile.getName(),
+				pluginDescriptionFile.getVersion())
+		);
+	}
 
-     }
+	public void onEnable() {
 
-    public void registerEvents() {
-        getServer().getPluginManager().registerEvents(this, this);
-    }
+		SQLHandler.setupMySQL();
 
-    public void setInstance() {
-        instance = this;
-    }
+		registerCommands();
+		registerHandlers();
+		registerEvents();
+	}
 
-    @SuppressWarnings("unchecked")
+	public void onDisable() {
+		SQLHandler.cleanupConnection();
+	}
+
+	public void registerCommands() {
+
+		getCommand("urestrictor").setExecutor(new RestrictorCommands());
+
+	}
+
+	public void registerHandlers() {
+
+		if (settings.getSQLData().getConfigurationSection("MySQL") == null) {
+			settings.getSQLData().set("MySQL.Enabled", false);
+			settings.getSQLData().set("MySQL.IP", "ip");
+			settings.getSQLData().set("MySQL.Port", "port");
+			settings.getSQLData().set("MySQL.Database", "database");
+			settings.getSQLData().set("MySQL.User", "user");
+			settings.getSQLData().set("MySQL.Password", "pass");
+			settings.saveSQLData();
+		}
+
+		if (settings.getPlayerData().getList("KnownUsernames") == null) {
+			settings.getPlayerData().set("KnownUsernames", new ArrayList<>());
+			settings.savePlayerData();
+		}
+		if (settings.getPlayerData().getString("KickMessage") == null) {
+			settings.getPlayerData().set("KickMessage", "&cYou may not change your name! Original name: &b%name%");
+		}
+
+	}
+
+	public void registerEvents() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	public void setInstance() {
+		instance = this;
+	}
+
+	@SuppressWarnings("unchecked")
 	@EventHandler(priority = EventPriority.LOWEST)
-    public void onLogin(AsyncPlayerPreLoginEvent event) {
-        String playerName = event.getName();
-        
-        List<String> names = settings.getPlayerData().getStringList("KnownUsernames");
-        
-        if (settings.getSQLData().getBoolean("MySQL.Enabled")) {
-        	try {
-    			PreparedStatement sql = SQLHandler.connection.prepareStatement("SELECT `username` FROM `knownUsernames`");
-    			
-    			ResultSet result = sql.executeQuery();
-    			
-    			while (result.next()) {
-    				
-    				String username = result.getString(1);
-    				
-    				if (isCaseInsensitive(playerName, username)) {
-    	                if (!username.equals(playerName)) {
-    	                    event.setKickMessage(ChatColor.translateAlternateColorCodes('&', settings.getPlayerData().getString("KickMessage").replace("%name%", username)));
-    	                    event.setLoginResult(Result.KICK_OTHER);
-    	                    break;
-    	                }
-    	            } else {
-    	                SQLHandler.addUsername(playerName);
-    	            }
-    				
-    			}
-    			
-        	}catch (Exception e) {
-        		e.printStackTrace();
-        	}
-        }
-        if (settings.getPlayerData().getList("KnownUsernames").isEmpty()) {
-            names.add(playerName);
-            settings.getPlayerData().set("KnownUsernames", names);
-            settings.savePlayerData();
-        }
-        for (String username : (List<String>) settings.getPlayerData().getList("KnownUsernames")) {
-            if (isCaseInsensitive(playerName, username)) {
-                if (!username.equals(playerName)) {
-                    event.setKickMessage(ChatColor.translateAlternateColorCodes('&', settings.getPlayerData().getString("KickMessage").replace("%name%", username)));
-                    event.setLoginResult(Result.KICK_OTHER);
-                    break;
-                }
-            } else {
-                names.add(playerName);
-                settings.getPlayerData().set("KnownUsernames", names);
-                settings.savePlayerData();
-            }
-        }
+	public void onLogin(AsyncPlayerPreLoginEvent event) {
+		String playerName = event.getName();
 
-    }
+		List<String> names = settings.getPlayerData().getStringList("KnownUsernames");
 
-    private boolean isCaseInsensitive(String str1, String str2) {
-        if (str1.equalsIgnoreCase(str2)) return true;
-        else return false;
-    }
+		if (settings.getSQLData().getBoolean("MySQL.Enabled")) {
+			try {
+				PreparedStatement sql = SQLHandler.connection.prepareStatement("SELECT `username` FROM `knownUsernames`");
+
+				ResultSet result = sql.executeQuery();
+
+				while (result.next()) {
+
+					String username = result.getString(1);
+
+					if (playerName.equalsIgnoreCase(username)) {
+						if (!username.equals(playerName)) {
+							event.setKickMessage(ChatColor.translateAlternateColorCodes('&', settings.getPlayerData().getString("KickMessage").replace("%name%", username)));
+							event.setLoginResult(Result.KICK_OTHER);
+							break;
+						}
+					} else {
+						SQLHandler.addUsername(playerName);
+					}
+
+				}
+
+				result.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			if (settings.getPlayerData().getList("KnownUsernames").isEmpty()) {
+				names.add(playerName);
+				settings.getPlayerData().set("KnownUsernames", names);
+				settings.savePlayerData();
+			}
+			for (String username : (List<String>) settings.getPlayerData().getList("KnownUsernames")) {
+				if (playerName.equalsIgnoreCase(username)) {
+					if (!username.equals(playerName)) {
+						event.setKickMessage(ChatColor.translateAlternateColorCodes('&', settings.getPlayerData().getString("KickMessage").replace("%name%", username)));
+						event.setLoginResult(Result.KICK_OTHER);
+						break;
+					}
+				} else {
+					names.add(playerName);
+					settings.getPlayerData().set("KnownUsernames", names);
+					settings.savePlayerData();
+				}
+			}
+		}
+
+	}
 }
